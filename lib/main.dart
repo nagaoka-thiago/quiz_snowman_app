04196b7b8994c6904bba40f32e7279057f92d318
Thiago Nagaoka <nagaoka.thiago@bol.com.br>
2022-05-05 10:53:59 -0300

    Fixed keyboard bug and fixed textfields layout.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:quiz_snowman_app/pages/main_page.dart';
 
 import 'pages/login_page.dart';
 
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      home: LoginPageWidget(),
     );
   }
 }
