eb72d2a279c347d909da9d0448033fd55bb53032
Thiago Nagaoka <nagaoka.thiago@bol.com.br>
2022-05-06 11:09:15 -0300

    adapted lists to add it to api url.

--- lib/functions/questions.dart
+++ lib/functions/questions.dart
@@ -2,27 +2,19 @@ import 'package:dio/dio.dart';
 
 import '../models/question_api.dart';
 
-Future<List<QuestionApi>> testandoApi() async {
+Future<List<QuestionApi>> getQuestions(
+    String categories, String limit, String difficulty) async {
   Response response = await Dio().get(
-      'https://the-trivia-api.com/api/questions?categories=arts_and_literature,film_and_tv,food_and_drink&limit=8&difficulty=easy');
+      'https://the-trivia-api.com/api/questions?' +
+          (categories.isNotEmpty ? 'categories=' + categories + '&' : '') +
+          'limit=' +
+          limit +
+          '&difficulty=' +
+          difficulty);
   if (response.statusCode == 200) {
     List listJson = (response.data as List);
+    print(listJson.length);
     return listJson.map((json) => QuestionApi.fromJson(json)).toList();
   }
   return Future.value([]);
 }
-/*return FutureBuilder(
-        future: testandoApi(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            List<QuestionApi> listQuestions =
-                snapshot.data as List<QuestionApi>;
-            return ListView.builder(
-                itemCount: listQuestions.length,
-                itemBuilder: (context, i) {
-                  return Text(
-                      listQuestions[i].incorrectAnswers!.length.toString());
-                });
-          }
-          return Text('Deu erro!');
-        });*/
