06b0d0df827a5c2b8098a452486ea190cf2a795b
Rodi444 <96850587+Rodi444@users.noreply.github.com>
2022-05-09 13:29:22 -0300

    url_update
    url within a variale

--- lib/functions/questions.dart
+++ lib/functions/questions.dart
@@ -4,13 +4,13 @@ import '../models/question_api.dart';
 
 Future<List<QuestionApi>> getQuestions(
     String categories, String limit, String difficulty) async {
-  Response response = await Dio().get(
-      'https://the-trivia-api.com/api/questions?' +
-          (categories.isNotEmpty ? 'categories=' + categories + '&' : '') +
-          'limit=' +
-          limit +
-          '&difficulty=' +
-          difficulty);
+  final url = 'https://the-trivia-api.com/api/questions?' +
+      (categories.isNotEmpty ? 'categories=' + categories + '&' : '') +
+      'limit=' +
+      limit +
+      '&difficulty=' +
+      difficulty;
+  final response = await Dio().get(url);
   if (response.statusCode == 200) {
     List listJson = (response.data as List);
     return listJson.map((json) => QuestionApi.fromJson(json)).toList();
