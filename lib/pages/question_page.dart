a7ac1b860b539176f0ff46992be7931510fcf8f9
Rodi444 <96850587+Rodi444@users.noreply.github.com>
2022-05-11 10:44:20 -0300

    quiz_pages_layout
    small slayout ajustments in quiz pages

--- lib/pages/question_page.dart
+++ lib/pages/question_page.dart
@@ -54,134 +54,141 @@ class _QuestionPageWidgetState extends State<QuestionPageWidget> {
     return SafeArea(
       child: Scaffold(
           backgroundColor: const Color.fromARGB(255, 191, 126, 174),
-          body: FutureBuilder<List<QuestionApi>>(
-              future: widget.questions,
-              builder: (context, snapshot) {
-                List<QuestionApi> questions = [];
-                List<String> alternatives = [];
-
-                if (snapshot.hasData) {
-                  questions = snapshot.data!;
-                  alternatives = [
-                    questions[current - 1].correctAnswer!,
-                    ...questions[current - 1].incorrectAnswers!
-                  ];
-
-                  final reducedButtonStatusList = _buttonStatus.toSet();
-                  if (reducedButtonStatusList.length == 1) {
-                    alternatives.shuffle();
-                  }
-                  return SingleChildScrollView(
-                    child: Container(
-                        height: MediaQuery.of(context).size.height,
-                        margin:
-                            const EdgeInsets.only(top: 20, left: 20, right: 20),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            LinearPercentIndicator(
-                              percent: current / questions.length,
-                              lineHeight: 24,
-                              progressColor:
-                                  const Color.fromARGB(255, 242, 169, 80),
-                              backgroundColor: Colors.white,
-                            ),
-                            const SizedBox(height: 16),
-                            Container(
-                              padding: const EdgeInsets.all(16),
-                              margin: const EdgeInsets.only(bottom: 20),
-                              width: MediaQuery.of(context).size.width * 0.9,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(25),
-                                color: const Color.fromRGBO(101, 48, 217, 0.37),
+          body: SingleChildScrollView(
+            child: FutureBuilder<List<QuestionApi>>(
+                future: widget.questions,
+                builder: (context, snapshot) {
+                  List<QuestionApi> questions = [];
+                  List<String> alternatives = [];
+                
+                  if (snapshot.hasData) {
+                    questions = snapshot.data!;
+                    alternatives = [
+                      questions[current - 1].correctAnswer!,
+                      ...questions[current - 1].incorrectAnswers!
+                    ];
+                
+                    final reducedButtonStatusList = _buttonStatus.toSet();
+                    if (reducedButtonStatusList.length == 1) {
+                      alternatives.shuffle();
+                    }
+                    return SingleChildScrollView(
+                      child: Container(
+                          height: MediaQuery.of(context).size.height,
+                          margin:
+                              const EdgeInsets.only(top: 20, left: 20, right: 20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              LinearPercentIndicator(
+                                percent: current / questions.length,
+                                lineHeight: 24,
+                                curve: Curves.easeOutCirc,
+                                barRadius: const Radius.circular(16),
+                                animationDuration: 500,
+                                animateFromLastPercent: true,
+                                animation: true,
+                                progressColor:
+                                    const Color.fromARGB(255, 242, 169, 80),
+                                backgroundColor: Colors.white,
                               ),
-                              child: Column(children: [
-                                Text('Question ' + current.toString(),
+                              const SizedBox(height: 16),
+                              Container(
+                                padding: const EdgeInsets.all(16),
+                                margin: const EdgeInsets.only(bottom: 20),
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(25),
+                                  color: const Color.fromRGBO(101, 48, 217, 0.37),
+                                ),
+                                child: Column(children: [
+                                  Text('QUESTION ' + current.toString(),
+                                      style: GoogleFonts.robotoMono(
+                                          fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold)),
+                                  const SizedBox(height: 20),
+                                  Text(
+                                    questions[current - 1].question!,
                                     style: GoogleFonts.robotoMono(
-                                        fontSize: 20, color: Colors.white)),
-                                const SizedBox(height: 20),
-                                Text(
-                                  questions[current - 1].question!,
-                                  style: GoogleFonts.robotoMono(
-                                      fontSize: 20, color: Colors.white),
-                                  textAlign: TextAlign.justify,
-                                )
-                              ]),
-                            ),
-                            SizedBox(
-                              height: 450,
-                              child: ListView.builder(
-                                  itemCount: alternatives.length,
-                                  itemBuilder: (context, i) {
-                                    // alternatives.shuffle();
-                                    return Container(
-                                      margin: const EdgeInsets.only(bottom: 10),
-                                      child: GlobalButton(
-                                          status: _buttonStatus[i],
-                                          onPressed: () async {
-                                            if (current <= questions.length) {
-                                              if (alternatives[i] ==
-                                                  questions[current - 1]
-                                                      .correctAnswer) {
-                                                setState(() {
-                                                  _buttonStatus[i] =
-                                                      ButtonStatus.correct;
-                                                  correctAnswers++;
-                                                });
-                                              } else {
-                                                setState(() {
-                                                  _buttonStatus[i] =
-                                                      ButtonStatus.wrong;
-                                                });
+                                        fontSize: 20, color: Colors.white),
+                                    textAlign: TextAlign.justify,
+                                  )
+                                ]),
+                              ),
+                              SizedBox(
+                                height: 450,
+                                child: ListView.builder(
+                                    itemCount: alternatives.length,
+                                    itemBuilder: (context, i) {
+                                      // alternatives.shuffle();
+                                      return Container(
+                                        margin: const EdgeInsets.only(bottom: 10),
+                                        child: GlobalButton(
+                                            status: _buttonStatus[i],
+                                            onPressed: () async {
+                                              if (current <= questions.length) {
+                                                if (alternatives[i] ==
+                                                    questions[current - 1]
+                                                        .correctAnswer) {
+                                                  setState(() {
+                                                    _buttonStatus[i] =
+                                                        ButtonStatus.correct;
+                                                    correctAnswers++;
+                                                  });
+                                                } else {
+                                                  setState(() {
+                                                    _buttonStatus[i] =
+                                                        ButtonStatus.wrong;
+                                                  });
+                                                }
+                                                await Future.delayed(
+                                                    const Duration(seconds: 1));
+                                                _resetButtons();
+                                                if (current + 1 <=
+                                                    questions.length) {
+                                                  setState(() {
+                                                    current++;
+                                                  });
+                                                } else {
+                                                  FirebaseFirestore.instance
+                                                      .collection('users')
+                                                      .doc(widget.user.sId)
+                                                      .update({
+                                                    "quizes":
+                                                        FieldValue.arrayUnion([
+                                                      {
+                                                        DateTime.now().toString():
+                                                            correctAnswers /
+                                                                questions.length
+                                                      }
+                                                    ])
+                                                  });
+                                                  Navigator.push(
+                                                      context,
+                                                      MaterialPageRoute(
+                                                          builder: (context) =>
+                                                              ScorePageWidget(
+                                                                  user:
+                                                                      widget.user,
+                                                                  score: correctAnswers /
+                                                                      questions
+                                                                          .length)));
+                                                }
                                               }
-                                              await Future.delayed(
-                                                  const Duration(seconds: 1));
-                                              _resetButtons();
-                                              if (current + 1 <=
-                                                  questions.length) {
-                                                setState(() {
-                                                  current++;
-                                                });
-                                              } else {
-                                                FirebaseFirestore.instance
-                                                    .collection('users')
-                                                    .doc(widget.user.sId)
-                                                    .update({
-                                                  "quizes":
-                                                      FieldValue.arrayUnion([
-                                                    {
-                                                      DateTime.now().toString():
-                                                          correctAnswers /
-                                                              questions.length
-                                                    }
-                                                  ])
-                                                });
-                                                Navigator.push(
-                                                    context,
-                                                    MaterialPageRoute(
-                                                        builder: (context) =>
-                                                            ScorePageWidget(
-                                                                user:
-                                                                    widget.user,
-                                                                score: correctAnswers /
-                                                                    questions
-                                                                        .length)));
-                                              }
-                                            }
-                                          },
-                                          text: alternatives[i]),
-                                    );
-                                  }),
-                            ),
-                          ],
-                        )),
-                  );
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              })),
+                                            },
+                                            text: alternatives[i]),
+                                      );
+                                    }),
+                              ),
+                            ],
+                          )),
+                    );
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                }),
+          )),
     );
   }
 }
