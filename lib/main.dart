ca07a4d81ec7daf349f64f7d91fb33ecc93d12e7
rodolfo-kunzel <rodi.bkunzel@gmail.com>
2022-05-16 14:11:20 -0300

    notification
    sends a notification when the app opens

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:splashscreen/splashscreen.dart';
 
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  print(await FirebaseMessaging.instance.getToken());
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
 
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:quiz_snowman_app/pages/leaderboard_page.dart';
 import 'package:quiz_snowman_app/pages/profile_page.dart';
 import 'package:quiz_snowman_app/widgets/mainpage_widgets/main_body.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/user_api.dart';
 
@@ -16,12 +22,46 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  late AndroidNotificationChannel channel;
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
   int bottomSelectedIndex = 0;
+  // String? fcmToken;
 
   final PageController controller = PageController(
     initialPage: 0,
     keepPage: true,
   );
+
+  void loadFCM() async {
+    if (!kIsWeb) {
+      channel = const AndroidNotificationChannel(
+        'high_importance_channel', // id
+        'High Importance Notifications', // title
+        importance: Importance.high,
+      );
+
+      flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+      /// Create an Android Notification Channel.
+      ///
+      /// We use this channel in the `AndroidManifest.xml` file to override the
+      /// default FCM channel to enable heads up notifications.
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+              AndroidFlutterLocalNotificationsPlugin>()
+          ?.createNotificationChannel(channel);
+
+      /// Update the iOS foreground notification presentation options to allow
+      /// heads up notifications.
+      await FirebaseMessaging.instance
+          .setForegroundNotificationPresentationOptions(
+        alert: true,
+        badge: true,
+        sound: true,
+      );
+    }
+  }
 
   Widget buildPageView() {
     return PageView(
@@ -40,6 +80,38 @@ class _MainPageState extends State<MainPage> {
   @override
   void initState() {
     super.initState();
+    loadFCM();
+    listenFCM();
+    getToken();
+    FirebaseMessaging.instance.subscribeToTopic("quiz");
+    sendPushMessage();
+  }
+
+  void listenFCM() async {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null && !kIsWeb) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              // TODO add a proper drawable resource to android, for now using
+              //      one that already exists in example app.
+              icon: 'launch_background',
+            ),
+          ),
+        );
+      }
+    });
+  }
+
+  void getToken() async {
+    await FirebaseMessaging.instance.getToken().then((token) => print(token));
   }
 
   void pageChanged(int index) {
@@ -58,6 +130,36 @@ class _MainPageState extends State<MainPage> {
             duration: const Duration(milliseconds: 500), curve: Curves.ease);
       },
     );
+  }
+
+  void sendPushMessage() async {
+    try {
+      await http.post(
+        Uri.parse('https://fcm.googleapis.com/fcm/send'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization':
+              'key=AAAA2mxdJug:APA91bEkQ683Foa5NAiaErtm-7qrz0Osz-eBr9mIa54AJr9AxRKDaQ0FjAZOvWEDLBUhDSJThs6-ZVk6UyBs3dQKOtN50ROB0GQjpbH2X0vy5LnSmpHkl2-0u88BHqfv6A6KxWLquSFu',
+        },
+        body: jsonEncode(
+          <String, dynamic>{
+            'notification': <String, dynamic>{
+              'body': 'Welcome to the best quiz App in the world',
+              'title': 'HELLO'
+            },
+            'priority': 'high',
+            'data': <String, dynamic>{
+              'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+              'id': '1',
+              'status': 'done'
+            },
+            "to": "/topics/quiz",
+          },
+        ),
+      );
+    } catch (e) {
+      print("error push notification");
+    }
   }
 
   @override
